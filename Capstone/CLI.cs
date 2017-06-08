18732a700ab49aef14bb0593b89fe504ce491690
Kevin Somerville <ksomerville0101@gmail.com>
2017-06-08 10:14:56 -0400

    cli changes

--- Capstone/CLI.cs
+++ Capstone/CLI.cs
@@ -6,7 +6,94 @@ using System.Threading.Tasks;
 
 namespace Capstone
 {
-    class CLI
+    public class CLI
     {
+        const string Command_AllParks = "1";
+        const string Command_Park_Campgrounds = "2";
+        const string Command_Quit = "q";
+        readonly string DatabaseConnection = System.Configuration.ConfigurationManager.ConnectionStrings["CapstoneDatabase"].ConnectionString;
+
+        public void RunCLI()
+        {
+            PrintHeader();
+            PrintMenu();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                Console.Clear();
+
+                switch (command.ToLower())
+                {
+                    case Command_AllParks:
+                        GetAllParks();
+                        break;
+
+                    case Command_Park_Campgrounds:
+                        ViewParkCampgrounds();
+                        break;
+
+
+                    case Command_Quit:
+                        { 
+}
+                        Console.WriteLine("Thank you for using the project organizer");
+                        return;
+
+                    default:
+                        Console.WriteLine("The command provided was not a valid command, please try again.");
+                        break;
+
+                }
+
+                PrintMenu();
+            }
+        }
+
+        public void PrintHeader()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("************PARKS RESERVATION SYSTEM**************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Main Menu Please type in a command");
+            Console.WriteLine(" 1 - Show all parks");
+            Console.WriteLine(" 2 - Choose a park and view all campgrounds");
+
+        }
+
+        private void GetAllParks()
+        {
+            ParkSqlDAL dal = new ParkSqlDAL(DatabaseConnection);
+            List<Park> parks = dal.GetParks();
+
+            if (parks.Count > 0)
+            {
+                parks.ForEach(park =>
+                {
+                    Console.WriteLine(park);
+                });
+            }
+            else
+            {
+                Console.WriteLine("**** NO RESULTS ****");
+            }
+        }
+
+        private void ViewParkCampgrounds()
+        {
+            ParkSqlDAL dal = new ParkSqlDAL(DatabaseConnection);
+        }
+
+
     }
 }
+
+
