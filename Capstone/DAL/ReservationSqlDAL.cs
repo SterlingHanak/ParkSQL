24cd31e250b1889f7b0e846c754573b397ec573f
Sterling Hanak <hanak@roadrunner.com>
2017-06-09 09:57:29 -0400

    Wrote methods in the ReservationSqlDAL class

--- Capstone/DAL/ReservationSqlDAL.cs
+++ Capstone/DAL/ReservationSqlDAL.cs
@@ -13,13 +13,16 @@ namespace Capstone.DAL
     {
         private string connectionString;
         private const string SQL_IsCampgroundOpen = @"SELECT * FROM campground WHERE campground_id = @campground_id";
-        private const string SQL_GetAvailableSites = @"SELECT * FROM site INNER JOIN reservation ON reservation.site_id = site.site_id WHERE campground_id = @campground_id";
+        private const string SQL_GetAvailableSites = @"SELECT * FROM reservation";
+        private const string SQL_CreateReservation = @"INSERT INTO reservation (site_id, name, to_date, from_date, create_date) VALUES(@site_id, @name, @to_date, @from_date, @create_date)";
+        List<Reservation> allReservations = new List<Reservation>();
 
         // Single Parameter Constructor
         public ReservationSqlDAL(string dbConnectionString)
         {
             connectionString = dbConnectionString;
         }
+
 
         public bool IsCampgroundOpen(Campground camp, DateTime start_date, DateTime end_date)
         {
@@ -51,37 +54,78 @@ namespace Capstone.DAL
             return false;
         }
 
-        //public List<Site> GetAvailableSites(Site campsite, DateTime startDate, DateTime endDate)
-        //{
-        //    List<Site> availableSites = new List<Site>();
 
-        //    try
-        //    {
-        //        using (SqlConnection conn = new SqlConnection(connectionString))
-        //        {
-        //            conn.Open();
+        public List<Reservation> GetAvailableSites()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
-        //            SqlCommand cmd = new SqlCommand(SQL_GetAvailableSites, conn);
-        //            cmd.Parameters.AddWithValue("@campground_id", campsite.CampgroundId);
+                    SqlCommand cmd = new SqlCommand(SQL_GetAvailableSites, conn);
 
-        //            SqlDataReader reader = cmd.ExecuteReader();
+                    SqlDataReader reader = cmd.ExecuteReader();
 
-        //            while (reader.Read())
-        //            {
-        //                if (!(startDate >= from_date && startDate <= to_date) || !(endDate >= from_date && endDate <= to_date))
-        //                {
-        //                    availableSites.Add(PopulateSiteObject(reader));
-        //                }
-        //            }
+                    while (reader.Read())
+                    {
+                        allReservations.Add(PopulateReservationObject(reader));
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return allReservations;
+        }
 
-        //        }
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //        throw;
-        //    }
-        //}
+
+        public bool IsReservationOpen(DateTime startDate, DateTime endDate)
+        {
+            
+            foreach(Reservation party in allReservations)
+            {
+                if (startDate >= party.From_Date && startDate <= party.To_Date) return false;
+                if (endDate >= party.From_Date && endDate <= party.To_Date) return false;
+                if (startDate < party.From_Date && endDate > party.To_Date) return false;
+            }
+            return true;
+        }
+
+
+        public bool CreateReservation(Reservation newReservation)
+        {
+            bool isAvailable = IsReservationOpen(newReservation.From_Date, newReservation.To_Date);
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    if (isAvailable)
+                    {
+                        SqlCommand cmd = new SqlCommand(SQL_CreateReservation, conn);
+                        cmd.Parameters.AddWithValue("@site_id", newReservation.SiteId);
+                        cmd.Parameters.AddWithValue("@name", newReservation.Name);
+                        cmd.Parameters.AddWithValue("from_date", newReservation.From_Date);
+                        cmd.Parameters.AddWithValue("to_date", newReservation.To_Date);
+                        cmd.Parameters.AddWithValue("create_date", DateTime.Now);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        return (rowsAffected > 0);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return false;
+        }
+        
 
         public Reservation PopulateReservationObject(SqlDataReader reader)
         {
